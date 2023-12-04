b679a0a4964fea7e3459acb9c349a8a1f449e6e9
Anton Zimin <zimkaa87@gmail.com>
2023-12-04 22:49:38 +0500

    fix: example file

--- start2.py
+++ start2.py
@@ -1,13 +1,11 @@
-from logging import Logger
 from typing import NoReturn
-
 from pydantic import BaseModel
-
-
+from logging import Logger
+import time
 logger = Logger(__name__)
 my_dict = {
-    1: "apple",
-    2: "ball",
+    1: 'apple',
+    2: 'ball',
 }
 
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -21,18 +19,17 @@ class MyError(Exception):
 
 
 def do_example(a: str | None = None, b: int | None = None, c: int | None = None, d: int | None = None, e: int | None = None) -> NoReturn:
-    """Compute function."""
+    """Compute function"""
     # logger.info(a, b, c, d, e)
     msg = "no way"
     raise MyError(msg)
 
 
 def do_example2(a: str | None = None, b: int | None = None, c: int | None = None, d: int | None = None, e: int | None = None, f: int | None = None) -> NoReturn:
-    """Example function2."""
+    """Example function2"""
     logger.info(a, b, c, d, e, f)
 
-def do_example3(a: str | None = None, b: int | None = None, c: int | None = None, d: int | None = None, e: int | None = None) -> NoReturn:
-    """Example function."""
+def do_example3(a: str | None = None, b: int | None = None, c: int | None = None, d: int | None = None, e: int | None = None,) -> NoReturn:
+    """Example function"""
     logger.info(a, b, c, d, e)
-    msg = "no way"
-    raise Exception(msg)
+    raise Exception('no way')
